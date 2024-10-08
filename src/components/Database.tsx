5b1a8d45662d4ab153b9d75e3c94fae337098407
Steve <stevedsimkins@gmail.com>
2024-10-08 12:00:35 -0400

    added local idb

--- src/components/Database.tsx
+++ src/components/Database.tsx
@@ -23,6 +23,28 @@ export default function Database() {
 	const [saving, setSaving] = useState(false);
 	const { toast } = useToast();
 
+	async function checkDatabaseExists(dbName: string): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			const request = indexedDB.open(dbName);
+
+			request.onsuccess = (event) => {
+				const db = (event.target as IDBOpenDBRequest).result;
+				db.close();
+				resolve(true);
+			};
+
+			request.onerror = (event) => {
+				reject(new Error("Error opening database"));
+			};
+
+			request.onupgradeneeded = (event) => {
+				const db = (event.target as IDBOpenDBRequest).result;
+				db.close();
+				resolve(false);
+			};
+		});
+	}
+
 	async function importDb() {
 		try {
 			setLoading(true);
@@ -32,9 +54,21 @@ export default function Database() {
 				functionName: "getState",
 			});
 			console.log(dbCid);
-			const dbFile = await pinata.gateways.get(dbCid as unknown as string);
-			const file = dbFile.data as Blob;
-			db = new PGlite({ loadDataDir: file });
+			const dbExists = await checkDatabaseExists("todo-db");
+			if (!dbExists) {
+				const dbFile = await pinata.gateways.get(dbCid as unknown as string);
+				const file = dbFile.data as Blob;
+				db = new PGlite({
+					loadDataDir: file,
+					dataDir: "idb://todo-db",
+				});
+				console.log("used remote db");
+			} else {
+				db = new PGlite({
+					dataDir: "idb://todo-db",
+				});
+				console.log("used local db");
+			}
 			const ret = await db?.query(`
 				SELECT * from todo ORDER BY id ASC;
 			`);
